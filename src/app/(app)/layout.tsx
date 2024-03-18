c2c00cddef14ddc8a95a5de3e49172302e840948
Diego Ramos <diegorbrito9@gmail.com>
2024-03-18 09:35:29 -0300

    chore: limpando o projeto

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+import './globals.css';
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'] });
+
+export const metadata: Metadata = {
+  title: 'Youtube downloader video',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
